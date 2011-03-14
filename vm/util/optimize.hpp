9c784f483b357d57f4bba90900099a2879746fdd
wanabe <s.wanabe@gmail.com>
2011-03-14 21:40:40 +0900

    sizeof(void*) == sizeof(long long int) > sizeof(long int) on LLP64.

--- vm/util/optimize.hpp
+++ vm/util/optimize.hpp
@@ -4,8 +4,13 @@
 // keeps the normal case fast.
 #ifdef __GNUC__
 
+#ifdef _WIN64
+#define likely(x)       __builtin_expect((long long int)(x),1)
+#define unlikely(x)     __builtin_expect((long long int)(x),0)
+#else
 #define likely(x)       __builtin_expect((long int)(x),1)
 #define unlikely(x)     __builtin_expect((long int)(x),0)
+#endif
 
 #else
 
